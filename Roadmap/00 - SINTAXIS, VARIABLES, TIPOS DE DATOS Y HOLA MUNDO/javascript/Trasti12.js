bbcf66333f87ec7b2ec71233f94f59429f2e3e31
Trasti M <120570018+Trasti12@users.noreply.github.com>
2024-06-12 07:18:16 -0500

    Trasti12.js

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/Trasti12.js
@@ -0,0 +1,62 @@
+
+
+// 1. Crear un comentario en el código y colocar la URL del sitio web oficial del lenguaje de programación que has seleccionado.
+
+  //https://developer.mozilla.org/en-US/docs/Web/JavaScript
+
+
+
+// 2. Representar las diferentes sintaxis que existen de crear comentarios en el lenguaje (en una línea, varias...).
+
+  // Comentario de una sola línea
+
+  /* Comentarios extensos 
+  o   
+  de múltiples líneas */
+
+
+
+// 3. Crear una variable (y una constante si el lenguaje lo soporta).
+
+  // Variable : Es un elemento de datos con nombre cuyo valor puede cambiar durante el curso de la ejecución de un programa.
+        
+        let dia = Lunes 
+
+  // Constante : Es un número, un carácter o una cadena de caracteres que se puede utilizar como valor en un programa. 
+    
+        const letra = T
+
+/* Crear variables representando todos los tipos de datos primitivos (son datos que no son un objeto y no tienen métodos) del lenguaje 
+   (cadenas de texto, enteros, booleanos...).*/
+
+    /*  Indefinido : Es una variable a la que no se le ha asignado valor. Un método o sentencia también devuelve undefined si la variable 
+        que se está evaluando no tiene asignado un valor*/
+    
+            let Undefined = indefinido
+
+    //  Booleano : Es un dato lógico que solo puede tener los valores true o false.
+     
+            let Boolean = false
+
+    //  Numero : Es un objeto primitivo envolvente que permite representar y manipular valores numéricos cómo 37 o -9.25.
+     
+            let Number = 9
+
+    //  String : Es una secuencia de caracteres usado para representar un texto.
+     
+            let String = "Hola tú"
+
+    //  BigInt : Es un tipo de dato numerico que puede representar números enteros en el formato de precision arbitrario. 
+     
+            let BigInt = 16989657
+
+    /*  Symbol: Es un tipo de datos cuyos valores son únicos e immutables. Los Symbols nos permiten crear propiedades “ocultas” en un 
+        objeto, a las cuales ninguna otra parte del código puede acceder ni sobrescribir. */
+
+            let id = Symbol ("id")
+
+
+            
+// Imprimir por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+
+      console.log("Hi JavaScript");
